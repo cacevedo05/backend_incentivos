2931fbc63661252c2465f688770c7eeb661c2c4d
cacevedo <cacevedo@stop.com.co>
2026-05-27 10:29:03 -0500

    ajustes ip3

--- services/auth/src/modules/auth/auth.service.ts
+++ services/auth/src/modules/auth/auth.service.ts
@@ -9,14 +9,14 @@ export class AuthService {
   async login(data: LoginDTO) {
     const user = await this.repo.findByEmail(data.email)
     if (!user) {
-      throw new Error('Credenciales inválidas')
+      throw new Error('TUS Credenciales inválidas')
     }
     if (!user.active) {
       throw new Error('El usuario no está activo')
     }
     const isPasswordValid = await bcrypt.compare(data.password, user.password)
     if (!isPasswordValid) {
-      throw new Error('Credenciales inválidas')
+      throw new Error('TUS Credenciales inválidas')
     }
     const token = generateToken({ id: user.id, role: user.role })
     return {
